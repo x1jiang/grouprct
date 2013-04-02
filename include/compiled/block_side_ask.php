b9b533331972e54d74c6cc28000db3039785dc97
xiaoqian.jiang@gmail.com <xiaoqian.jiang@gmail.com@f226211a-b2b1-d446-b253-1ccd8546464a>
2013-04-02 22:37:30 +0000

    updated a few logos

--- include/compiled/block_side_ask.php
+++ include/compiled/block_side_ask.php
@@ -8,14 +8,14 @@
 	<div class="sbox-top"></div>
 	<div class="sbox-content">
 		<div class="deal-consult-tip">
-			<h2>Query</h2>
+			<h2>Online Q&A</h2>
 			<p class="nav"><a href="/team/ask.php?id=<?php echo $team['id']; ?>">View all (<?php echo $ask_count; ?>)</a> | <a href="/team/ask.php?id=<?php echo $team['id']; ?>#post">Raise a question</a></p>
 			<ul class="list">
 			<?php if(is_array($asks)){foreach($asks AS $one) { ?>
 				<li><a href="/team/ask.php?id=<?php echo $team['id']; ?>#ask-entry-<?php echo $one['id']; ?>" target="_blank"><?php echo htmlspecialchars(mb_substr($one['content'],0,30,'UTF-8')); ?>...</a></li>
 			<?php }}?>
 			</ul>
-			<div class="consume-service"><p class="im"><a id="service-msn-help" href="msnim:chat?contact=<?php echo $INI['system']['kefumsn']; ?>"><img src="/static/css/i/button-consume-msn.gif" /></a>&nbsp;<a href="tencent://message/?uin=<?php echo $INI['system']['kefuqq']; ?>&Site=<?php echo $INI['system']['sitename']; ?>&Menu=yes"><img SRC="/static/css/i/button-consume-qq.gif" alt=""></a></p><p class="time">Monday-Saturday 9:00-18:00</p></div>
+			<div class="consume-service"><p class="im"><a id="service-msn-help" href="msnim:chat?contact=<?php echo $INI['system']['kefumsn']; ?>"><img src="/static/css/i/button-consume-msn.gif" /></a>&nbsp;<a href="tencent://message/?uin=<?php echo $INI['system']['kefuqq']; ?>&Site=<?php echo $INI['system']['sitename']; ?>&Menu=yes"></a></p><p class="time">Monday-Friday 9:00am-5:00pm PST</p></div>
 		</div>
 	</div>
 	<div class="sbox-bottom"></div>
